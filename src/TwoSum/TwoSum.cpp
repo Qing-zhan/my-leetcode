7f9e57368c4ef7223f489772fe2ce86dc0e2796d
satchel <1763135013@qq.com>
2018-05-14 15:45:09 +0800

    implement twoSum

--- src/TwoSum/TwoSum.cpp
+++ src/TwoSum/TwoSum.cpp
@@ -4,12 +4,24 @@ using namespace std;
 
 class Solution {
 public:
-	vector<int> twoSum(vector<int>& nums, int target){
-		
+	vector<int> twoSum(vector<int>& nums, int target) {
+	  vector<int> result(2,0);
+	  int size = nums.size();
+	  for (int i = 0; i < size; i++) {
+	    for (int j = i + 1; j < size; j++) {
+	      if (nums[i] + nums[j] == target)
+	        result[0] = i;
+	        result[1] = j;
+	    }
+	  }
+	  return result;
 	}
 };
 
 int main(){
-   int n = 10;
-   cout << n << endl;
+  vector<int> nums_1(2, 7, 11, 15);
+  vector<int> result;
+  result = twoSum(nums_1, 9);
+
+  return 0
 }
